e4bb3581896f828105b2f10c6ca962a659fc55d2
mjarzebkowski <maciej.jarzebkowski@gmail.com>
2024-10-06 20:57:48 +0200

    Update Program.cs

--- dotnet-base-template/Program.cs
+++ dotnet-base-template/Program.cs
@@ -1,36 +1,79 @@
 
+using Serilog;
+
 namespace dotnet_base_template
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly string CurrentEnvironment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+        public static Task<int> Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                ConfigureLogging();
+                Log.Information($"Starting service worker. Environment:{CurrentEnvironment}");
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Add services to the container.
+
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
+
+                return Task.FromResult(0);
             }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+                Log.Fatal(ex, "Service worker terminated unexpectedly");
+                return Task.FromResult(1);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
 
-            app.UseHttpsRedirection();
+        private static IConfigurationBuilder SetupCunfiguration(IConfigurationBuilder configBuilder)
+        {
+            configBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            configBuilder.AddJsonFile(
+                $"appsettings.{CurrentEnvironment}.json",
+                optional: true);
+            configBuilder.AddEnvironmentVariables();
+            return configBuilder;
+        }
 
-            app.UseAuthorization();
+        private static void ConfigureLogging()
+        {
+            var configuration = SetupCunfiguration(new ConfigurationBuilder())
+                .Build();
 
-
-            app.MapControllers();
-
-            app.Run();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
         }
     }
 }
